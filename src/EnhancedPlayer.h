16e8931af940333e43160ce064bfcc926597f9b3
Sriteja Sugoor <sritejakittu777@gmail.com>
2019-01-23 02:24:39 +0530

    added new fields

--- src/EnhancedPlayer.h
+++ src/EnhancedPlayer.h
@@ -18,27 +18,44 @@ public:
     glm::vec3 position;
     Ball head;
     float speed;
-    float hand_rotation_angle;
-
+    float upward_acceleration;
+    float downward_acceleration;
+    float max_vertical_velocity;
+    float vertical_velocity;
+    float min_vertical_velocity;
+    float horizontal_velocity;
+    float horizontal_deceleration;
+    float horizontal_acceleration;
 
     IrregularPolygon body;
-    IrregularPolygon foot;
     IrregularPolygon left_hand;
     IrregularPolygon right_hand;
     IrregularPolygon left_leg;
     IrregularPolygon right_leg;
+    IrregularPolygon jet_box;
+    IrregularPolygon fire[5];
 
     void move_right();
-
-    float rotation_speed;
-
     void move_left();
+    void move_up();
+    void move_down();
+
+    void update_position_x(float x);
+    void update_position_y(float y);
+//    void get_position();
+    float net_vertical_acceleration;
+    float rotation_speed;
+    bool moving_up;
+    bool glow_fire;
 
 private:
     bool hand_going_up;
     bool moving_left;
+    bool moving_right;
     float leg_rotation_speed;
+
 //    void reset_rotation_values();
+
 };
 
 
