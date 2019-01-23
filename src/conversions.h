5aae8c852a6f662dc14e2d66e893977236303aa1
Sriteja Sugoor <sritejakittu777@gmail.com>
2019-01-23 20:39:35 +0530

    header file for conversion functions

---
+++ src/conversions.h
@@ -0,0 +1,19 @@
+//
+// Created by sriteja on 23/1/19.
+//
+
+#ifndef GRAPHICS_ASGN1_CONVERSIONS_H
+#define GRAPHICS_ASGN1_CONVERSIONS_H
+
+#include "main.h"
+#include "ball.h"
+#include "EnhancedPlayer.h"
+//#include ""
+
+
+CircleObject convert_ball_circle_object(Ball one);
+RectangleObject convert_player_rectangle(EnhancedPlayer one);
+
+
+
+#endif //GRAPHICS_ASGN1_CONVERSIONS_H
