99333479e490ee55960c1f90b6a8ded629754941
Sriteja Sugoor <sritejakittu777@gmail.com>
2019-05-06 14:53:58 +0530

    File for displaying digits

---
+++ src/display_number.cpp
@@ -0,0 +1,21 @@
+//
+// Created by sriteja on 26/1/19.
+//
+
+
+#include "main.h"
+#include "IrregularPolygon.h"
+
+
+void display_number() {
+    GLfloat one_coordinates[] = {
+            0.1f, 1.0f, 0.0f,
+            -0.1f, 1.0f, 0.0f,
+            -0.1f, -1.0f, 0.0f,
+            0.1f, -1.0f, 0.0f
+    };
+
+    GLfloat  two_coordinates[] = {
+
+    };
+}
